41df4039ffc8e5fc05677fde72c727709db73d03
wei1071427874 <1071427874@qq.com>
2017-08-03 17:34:40 +0800

    删a加d

--- ZDSoft.CR/ZDSoft.CR/Login.aspx.cs
+++ ZDSoft.CR/ZDSoft.CR/Login.aspx.cs
@@ -16,8 +16,8 @@ namespace ZDSoft.CR
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            int a = 1;
             int c = 2;
+            int d = 4;
         }
 
         protected void btnSever_Click(object sender, EventArgs e)
