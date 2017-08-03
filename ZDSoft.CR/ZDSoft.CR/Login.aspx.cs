7fa33eb013dac72cef62588a8e2d36b7e7e57a35
wei1071427874 <1071427874@qq.com>
2017-08-03 17:28:42 +0800

    删除int b,增加int c

--- ZDSoft.CR/ZDSoft.CR/Login.aspx.cs
+++ ZDSoft.CR/ZDSoft.CR/Login.aspx.cs
@@ -17,7 +17,7 @@ namespace ZDSoft.CR
         protected void Page_Load(object sender, EventArgs e)
         {
             int a = 1;
-            int b = 2;
+            int c = 2;
         }
 
         protected void btnSever_Click(object sender, EventArgs e)
